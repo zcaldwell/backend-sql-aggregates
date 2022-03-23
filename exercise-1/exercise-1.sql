fd12e29e2b0331ecffe54e4923d09b548598e97b
zcaldwell <mintyst.john@gmail.com>
2022-03-23 12:32:23 -0700

    firstdone

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -1,5 +1,26 @@
--- find the title and count of the top 10 most rented films
--- use COUNT to count each row
--- GROUP BY the film_id
--- ORDER in descending order
--- LIMIT 10
+
+SELECT
+  title,
+  COUNT (rental_date)
+FROM
+  film
+INNER JOIN
+  inventory
+ON 
+  film.film_id = inventory.film_id
+INNER JOIN
+  rental
+ON
+  inventory.inventory_id = rental.inventory_id
+GROUP BY
+  film.film_id
+ORDER BY 
+  count DESC
+LIMIT  
+  10
+
+
+
+
+
+

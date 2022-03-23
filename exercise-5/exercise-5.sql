c75289917f51f3025a9d7a335fd9fb345c2c2b16
zcaldwell <mintyst.john@gmail.com>
2022-03-23 14:32:32 -0700

    test 6 done

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,9 @@
 -- the total run time of all inventory items if they were all played back to back
+SELECT
+  SUM(length)  
+FROM
+  inventory
+INNER JOIN
+  film
+ON
+  film.film_id = inventory.film_id

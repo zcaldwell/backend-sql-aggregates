7efb8adf4fda8e715a2d4301dea1e48d10f83e63
zcaldwell <mintyst.john@gmail.com>
2022-03-23 12:58:03 -0700

    done up to 4

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,17 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT 
+  category.name,
+  AVG(length)
+FROM
+  film
+INNER JOIN
+  film_category
+ON
+  film.film_id = film_category.film_id
+INNER JOIN
+  category
+ON 
+  film_category.category_id = category.category_id
+GROUP BY
+  name

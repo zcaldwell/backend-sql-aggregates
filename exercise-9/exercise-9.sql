3b60520c64e4a62ef224ca482ad158fae8517801
zcaldwell <mintyst.john@gmail.com>
2022-03-23 15:29:04 -0700

    all done!

--- exercise-9/exercise-9.sql
+++ exercise-9/exercise-9.sql
@@ -1,2 +1,12 @@
 -- ROLLUP of amount made per year, month, day
 -- use ROLLUP and EXTRACT YEAR, MONTH, DAY from payment_date
+SELECT 
+  SUM(amount),
+  EXTRACT (YEAR from payment_date) AS y,
+  EXTRACT (MONTH from payment_date) AS m,
+  EXTRACT (DAY from payment_date) AS d
+FROM
+  payment
+GROUP BY
+  ROLLUP (y, m, d)
+
